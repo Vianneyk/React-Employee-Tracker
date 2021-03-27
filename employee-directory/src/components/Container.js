cd8a638d2b7087cac513f59417ba7b467528034f
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-03-27 22:51:03 +0000

    file modified and error fixed

--- employee-directory/src/components/Container.js
+++ employee-directory/src/components/Container.js
@@ -11,7 +11,7 @@ function Container(){
     }, [])
 
     async function getRandomUsers(){
-        const result = await axios.get('https://randomuser.me/api/?results=25&seed=seed')
+        const result = await axios.get('https://randomuser.me/api/?results=20&seed=seed')
         setUsers(result.data.results)
     }
 
@@ -37,28 +37,12 @@ function Container(){
         getSearchResults()
     }
 
-    function sortEmail(){
-        console.log('SORTING!!!!')
+    function sortName(){
         const sorted = users.sort( function(item1, item2){
-            if(item1.email < item2.email){
+            if(item1.name.last < item2.name.last){
                 return -1
             }
-            if (item1.email > item2.email){
-                return 1
-            }
-            return 0
-        })
-        console.log('Sorted List: ', sorted)
-        setUsers([...sorted])
-    }
-
-    function sortNumber(){
-        console.log('SORTING!!!!')
-        const sorted = users.sort( function(item1, item2){
-            if(item1.phone < item2.phone){
-                return -1
-            }
-            if (item1.phone > item2.phone){
+            if (item1.name.last > item2.name.last){
                 return 1
             }
             return 0
@@ -69,16 +53,15 @@ function Container(){
 
     return(
         <div className="container" style={{marginTop: "20px", marginBottom: "20px"}}>
-            {/* search function */}
             <div className="input-group mb-3 float-center">
                 <input value={search} onChange={handleInputChange} type="text" className="form-control" placeholder="Search Employee by Name" aria-label="Recipient's username" aria-describedby="button-addon2"/>
                 <button className="btn btn-outline-danger" onClick={clearSearch}><i class="fas fa-window-close"></i></button>
                 <button onClick={handleFormSubmit} className="btn btn-outline-primary" type="submit" id="button-addon2">Search</button>
             </div>
             <div style={{display: "flex", justifyContent: "center",  margin: "auto", color: "gray"}}>
-                <p><small>Hover over the Phone Number or Email to Sort Employees</small></p>
+                <p><small>Hover over the Name to Sort Employees Alphabetically</small></p>
             </div>
-            <Table list={users} sortEmail={sortEmail} sortNumber={sortNumber}/>
+            <Table list={users} sortName={sortName}/>
         </div>
     )
 }
