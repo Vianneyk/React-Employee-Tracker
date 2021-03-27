0f8c1bfd3faad80e4a36c664e3fe6575f06eb2ae
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-03-27 21:43:37 +0000

    footer file created

---
+++ employee-directory/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Footer () {
+    return (
+        <footer className="footer">
+          <p>Copyright © Vianney</p>
+
+        </footer>
+    )
+}
+
+export default Footer
