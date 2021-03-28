32dc4daf301122dec9822c4ed28271c58310a6fc
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-03-28 21:27:06 +0100

    added new functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,7 +7,7 @@ function Table(props) {
             <thead>
                 <tr>
                 <th>Image</th>
-                <th>Name<button className='filter' onClick={props.sortName}><i className="fas fa-filter"></i></button></th>
+                <th>Name<button className='filter' onClick={props.sortName}><i class="fas fa-sort-alpha-down"></i></button></th>
                 <th>Email</th>
                 <th>Phone Number</th>
                 <th>Location</th>
@@ -15,14 +15,16 @@ function Table(props) {
             </thead>
             <tbody>
                 {props.list.map(user => 
-                <tr style={{borderTop: "2px solid gray"}}>
+                <tr style={{borderTop: "0.2px solid gray"}}>
                 <td>
                     <img src={user.picture.thumbnail} alt="profile"/>
                 </td>
                 <td>{user.name.first} {user.name.last}</td>
-                <td>{user.email}</td>
+                <td>
+                    <a href={"mailto:" + user.email} target="__blank">{user.email}</a>
+                </td>
                 <td>{user.phone}</td>
-                <td>{user.dob.date}</td>
+                <td>{user.location.city}, {user.location.country}</td>
                 </tr>
                 )}
             </tbody>
